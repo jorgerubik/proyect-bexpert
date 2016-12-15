76d2a31eaf06b6f0f80cecce71d997b1398bd3d0
Fábrica de Soluciones RAK <RAK@MacBook-Pro-de-Fabrica-2.local>
2016-12-15 12:39:42 -0600

    details

---
+++ resources/js/detail.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+			/*Agrega productos*/
+			$(".btn-add").click(function(e) {
+				var pr_n = $(".prod-number");
+				var current_val = pr_n.val();
+				var increment = parseInt(current_val)+1;
+				pr_n.val("");
+				pr_n.val(increment);
+
+			});
+
+			/*Resta productos*/
+			$(".btn-remove").click(function(e) {
+				var pr_n = $(".prod-number");
+				var current_val = pr_n.val();
+				var remove;
+
+				if( current_val>1 ) {
+					remove = parseInt(current_val)-1;
+					pr_n.val("");
+					pr_n.val(remove);
+				}
+			});
+
+			/*Star rating*/
+            $('#rate-product').barrating({
+                theme: 'fontawesome-stars-o',
+                initialRating: 4.5
+            });
+		});
